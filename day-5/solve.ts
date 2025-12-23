29235d7b55d84b7682d97710603edb1d1125a7d6
nieves-mon <cunanan.nm@gmail.com>
2025-12-23 21:36:09 +0800

    Solve day 5 part 1

--- day-5/solve.ts
+++ day-5/solve.ts
@@ -7,15 +7,56 @@ const getInput = () => {
   return puzzleInput;
 };
 
-const partOne = (puzzleInput: string) => {};
-const partTwo = (puzzleInput: string) => {};
+const partOne = (ranges: string[], availableIDs: number[]) => {
+  const rangesObj: { min: number; max: number }[] = [];
+
+  for (const range of ranges) {
+    const nums = range.split("-").map((num) => parseInt(num));
+    const min = nums[0]!;
+    const max = nums[1]!;
+
+    rangesObj.push({ min, max });
+  }
+
+  let numFresh = 0;
+  for (const id of availableIDs) {
+    for (const { min, max } of rangesObj) {
+      if (id < min || id > max) continue;
+
+      numFresh++;
+      break;
+    }
+  }
+
+  return numFresh;
+};
+const partTwo = (ranges: string[], availableIDs: number[]) => {};
 
 const main = () => {
   const puzzleInput = getInput();
+  const lines = puzzleInput.split("\n");
+
+  const ranges = [];
+  const availableIDs = [];
+
+  let breakFound = false;
+  for (const line of lines) {
+    if (breakFound) {
+      availableIDs.push(parseInt(line));
+      continue;
+    }
+
+    if (!line.trim().length) {
+      breakFound = true;
+      continue;
+    }
+
+    ranges.push(line);
+  }
 
   console.table({
-    partOne: partOne(puzzleInput),
-    partTwo: partTwo(puzzleInput),
+    partOne: partOne(ranges, availableIDs),
+    partTwo: partTwo(ranges, availableIDs),
   });
 };
 
