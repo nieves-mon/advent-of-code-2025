fa5f88f649f7a56cb36dcc040a56eaf89ed4a88c
nieves-mon <cunanan.nm@gmail.com>
2025-12-23 19:44:35 +0800

    Solve day 4 part 2

--- day-4/solve.ts
+++ day-4/solve.ts
@@ -61,7 +61,71 @@ const partOne = (rows: string[]) => {
 
   return numAccessibleRolls;
 };
-const partTwo = (rows: string[]) => {};
+
+const partTwo = (rows: string[]) => {
+  let numAccessibleRolls = 0;
+  let hasRemoved = true;
+
+  while (hasRemoved) {
+    hasRemoved = false;
+
+    for (let i = 0; i < rows.length; i++) {
+      let row = rows[i]!;
+      const rowLen = row.length;
+
+      for (let j = 0; j < rowLen; j++) {
+        if (row[j] !== "@") continue;
+
+        let numAdjacentRolls = 0;
+
+        const upperRow = rows[i - 1];
+        const lowerRow = rows[i + 1];
+        const rightIdx = j + 1;
+        const leftIdx = j - 1;
+
+        const isRightEdge = rightIdx === rowLen;
+        const isLeftEdge = j === 0;
+
+        // left
+        if (!isLeftEdge && row[leftIdx] === "@") numAdjacentRolls++;
+
+        // right
+        if (!isRightEdge && row[rightIdx] === "@") numAdjacentRolls++;
+
+        if (upperRow) {
+          // upper
+          if (upperRow[j] === "@") numAdjacentRolls++;
+
+          // upperleft
+          if (!isLeftEdge && upperRow[leftIdx] === "@") numAdjacentRolls++;
+
+          // upperright
+          if (!isRightEdge && upperRow[rightIdx] === "@") numAdjacentRolls++;
+        }
+
+        if (lowerRow) {
+          // lower
+          if (lowerRow[j] === "@") numAdjacentRolls++;
+
+          // lowerleft
+          if (!isLeftEdge && lowerRow[leftIdx] === "@") numAdjacentRolls++;
+
+          // lowerright
+          if (!isRightEdge && lowerRow[rightIdx] === "@") numAdjacentRolls++;
+        }
+
+        if (numAdjacentRolls < 4) {
+          numAccessibleRolls++;
+          rows[i] = row.slice(0, j) + "x" + row.slice(j + 1);
+          row = rows[i]!;
+          hasRemoved = true;
+        }
+      }
+    }
+  }
+
+  return numAccessibleRolls;
+};
 
 const main = () => {
   const puzzleInput = getInput();
